a09b58234f102cb7f83e5d7d49629aa01f538870
fenirani <fenirani@live.com>
2025-03-30 17:02:18 -0400

    Add deletion effect and adjust speed

--- typing_effect.js
+++ typing_effect.js
@@ -2,13 +2,30 @@
 var i = 0;
 
 var txt =  "I am a Software Engineer";
-var speed = 50;
+var speed = 100;
+isDone= false;
 
 function typeWriter() {
-    if(i <txt.length) {
+    if(i <txt.length && isDone == false)  // Type
+    {
         document.getElementById("typingEffect").innerHTML += txt.charAt(i);
         i++;
+        if (i==txt.length)
+        {
+            isDone = true;
+        }
         setTimeout(typeWriter,speed);
+    }
+    else if (i != 0 && isDone == true) // Delete
+    {
+        document.getElementById("typingEffect").innerHTML = txt.slice(0,i);
+        i--;
+        if (i == 0)
+        {
+            document.getElementById("typingEffect").innerHTML = txt.slice(0,i); //delete 1 more character
+            isDone=false; 
+        }
+        setTimeout(typeWriter,speed)
     }
 }
 
