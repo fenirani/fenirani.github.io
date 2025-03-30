4a4f85b9fe94fc8c19d3116485d8cffe76d7bce2
fenirani <fenirani@live.com>
2025-03-30 17:32:28 -0400

    Add array to typingEffect, loop through array, slow down typing when word is finished

--- typing_effect.js
+++ typing_effect.js
@@ -1,32 +1,41 @@
 
 var i = 0;
+var j =0;
 
-var txt =  "I am a Software Engineer";
+var txt =  ["Software Engineer","Front End Developer", "Gamer", "Fanstasy Football Fan"];
 var speed = 100;
 isDone= false;
 
 function typeWriter() {
-    if(i <txt.length && isDone == false)  // Type
+    if(i <txt[j].length && isDone == false)  // Type
     {
-        document.getElementById("typingEffect").innerHTML += txt.charAt(i);
+        speed=100; //reset speed to normal
+        document.getElementById("typingEffect").innerHTML += txt[j].charAt(i);
         i++;
-        if (i==txt.length)
+        if (i==txt[j].length)
         {
             isDone = true;
+            speed = 2000; //slow down speed when the word is finished
         }
         setTimeout(typeWriter,speed);
     }
     else if (i != 0 && isDone == true) // Delete
     {
-        document.getElementById("typingEffect").innerHTML = txt.slice(0,i);
+        speed = 100; //reset speed to normal
+        document.getElementById("typingEffect").innerHTML = txt[j].slice(0,i);
         i--;
         if (i == 0)
         {
-            document.getElementById("typingEffect").innerHTML = txt.slice(0,i); //delete 1 more character
-            isDone=false; 
+            document.getElementById("typingEffect").innerHTML = txt[j].slice(0,i); //delete 1 more character
+            isDone=false;
+            j++;
+            if (j== txt.length)
+            {
+                j=0; // reset the 'loop'
+            }
         }
         setTimeout(typeWriter,speed)
-    }
+    }    
 }
 
 typeWriter();
