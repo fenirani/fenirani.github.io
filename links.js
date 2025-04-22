d5bbac49796487f985d089c7169720e064b2d522
fenirani <fenirani@live.com>
2025-04-22 11:34:50 -0400

    Add transform to bottom navigation

--- links.js
+++ links.js
@@ -7,10 +7,14 @@ function highlight(el) { // el is 'about' , 'experience', 'projects', or 'contac
     if (highlighted) {
         //if something is highlighted unhighlight
         document.getElementById(highlighted).style.fontSize = "1em";
+        document.getElementById(highlighted).style.padding = "0px 0px 0px 0ch"; 
+
         document.getElementById(highlighted_sect).style.display = "none";
 
     }
     document.getElementById(el).style.fontSize = "2em";
+    document.getElementById(el).style.padding = "0ch 0ch 0ch 15ch"
+    
     document.getElementById(sect).style.display = "block"; // el + sect = 'el-sect'
     console.log(sect) ;
     highlighted = el ;
