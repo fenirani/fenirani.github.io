5e3bd8e04c8c4d8e5e1a6042ccb7c371da672ca4
fenirani <fenirani@live.com>
2025-04-22 09:50:21 -0400

    Streamline link functions into one, show and hide sections based on click of the link, css all that

--- links.js
+++ links.js
@@ -1,8 +1,18 @@
-function about() {
-    document.getElementById("menu-tab-item").innerHTML = "hahaha it jebeno works :)";
-    document.getElementsByClassName("spread-out").style.color = "Blue";
-}
 
-function experience() {
-    document.getElementById("menu-tab-item").innerHTML = "Here is my Experience";
+var highlighted = 'about';
+var highlighted_sect = 'about-section';
+
+function highlight(el) { // el is 'about' , 'experience', 'projects', or 'contact'
+    let sect = el + "-section";
+    if (highlighted) {
+        //if something is highlighted unhighlight
+        document.getElementById(highlighted).style.fontSize = "1em";
+        document.getElementById(highlighted_sect).style.display = "none";
+
+    }
+    document.getElementById(el).style.fontSize = "2em";
+    document.getElementById(sect).style.display = "block"; // el + sect = 'el-sect'
+    console.log(sect) ;
+    highlighted = el ;
+    highlighted_sect = sect
 }
