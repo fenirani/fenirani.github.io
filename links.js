f7a2a217d5cc44e2110d9d65a32b7d5ef36bae9c
fenirani <fenirani@live.com>
2025-04-22 01:04:18 -0400

    Start to add functions for links

---
+++ links.js
@@ -0,0 +1,8 @@
+function about() {
+    document.getElementById("menu-tab-item").innerHTML = "hahaha it jebeno works :)";
+    document.getElementsByClassName("spread-out").style.color = "Blue";
+}
+
+function experience() {
+    document.getElementById("menu-tab-item").innerHTML = "Here is my Experience";
+}
